823698d199e22369c209bc7fb9147a31cb5d6739
Mark Adams <mark@thoughtbot.com>
2014-10-21 14:58:00 -0700

    Remove checkmark bullshit from prompt
    * I have no idea what I'm doing

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -90,14 +90,6 @@ _colored_path()           { echo "$(_grey "%c")" }
 _colored_git_branch()     { echo "$(_git_prompt_color "$(_git_prompt_info)")" }
 _colored_git_difference() { echo "$(_yellow "$(_git_difference_from_track)")" }
 
-_display_current_vim_mode() {
-  if [[ $VIMODE == 'vicmd' ]]; then
-    echo "$(_red "✘")"
-  else
-    echo "$(_green "✔")"
-  fi
-}
-
 function zle-line-init zle-keymap-select {
   VIMODE=$KEYMAP
   zle reset-prompt
@@ -110,4 +102,4 @@ function precmd {
   $(git status 2> /dev/null >! "/tmp/git-status-$$")
 }
 
-PROMPT='$(_bracket_wrap "$(_basic)$(_separate $(_colored_git_branch))$(_separate $(_colored_git_difference))")$(_display_current_vim_mode) '
+PROMPT='$(_bracket_wrap "$(_basic)$(_separate $(_colored_git_branch))$(_separate $(_colored_git_difference))")'
