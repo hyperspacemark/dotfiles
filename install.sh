e846850c14d0582acf435da0f4e8b86b705cb9d1
Mark Adams <mark@thoughtbot.com>
2014-10-21 14:32:59 -0700

    Add install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+files=(aliases gemrc gitconfig gitignore hushlogin lldbinit vimrc zlogin zshrc)
+
+for file in ${files[@]}; do
+  new_path="$HOME/.$file"
+  if [[ ! -f $new_path && ! -d $new_path ]]; then
+    ln -s $PWD/$file $new_path
+    echo "Linking $PWD/$file to $new_path"
+  fi
+done
