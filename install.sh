f382b15ba9b306e1acdfaa59ff00c71abb0c49bf
Keith Smiley <keithbsmiley@gmail.com>
2014-11-26 15:23:39 -0800

    Check if path exists

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ files=(aliases gemrc gitconfig gitignore hushlogin lldbinit vim vimrc zshrc)
 
 for file in ${files[@]}; do
   new_path="$HOME/.$file"
-  if [[ ! -f $new_path && ! -d $new_path ]]; then
+  if [[ ! -e $new_path ]]; then
     ln -s $PWD/$file $new_path
     echo "Linking $PWD/$file to $new_path"
   fi
