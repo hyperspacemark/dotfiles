f4b47921d14a9c3516e2188a282377c1084bb767
Mark Adams <mark.adams86@gmail.com>
2015-04-17 14:11:35 -0700

    Add .lldbinit that loads chisel

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-files=(aliases gemrc gitconfig gitignore hushlogin vim vimrc xvimrc zshrc)
+files=(aliases gemrc gitconfig gitignore hushlogin lldbinit vim vimrc xvimrc zshrc)
 
 for file in ${files[@]}; do
   new_path="$HOME/.$file"
