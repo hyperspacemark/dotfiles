c10920d077c5267610ae592b0804a5dba24e3237
Mark Adams <mark.adams86@gmail.com>
2015-10-17 18:55:01 -0700

    Fix git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,7 +1,14 @@
 alias reloadignore="g rm -r --cached . && g add -A"
+
 alias gcb="g create-branch"
 alias gdb="g delete-branch"
+
 alias gc="g checkout"
 alias gf="g fetch"
+
 alias gp="g push"
 alias gpl="g pull"
+
+alias grc="g rebase --continue"
+alias gra="g rebase --abort"
+alias grs="g rebase --skip"
