8ba27bf348e7851b35ac705e712556ce75287542
Mark Adams <mark.adams86@gmail.com>
2015-10-19 13:07:36 -0700

    Add 'play' alias

--- xcode/aliases.zsh
+++ xcode/aliases.zsh
@@ -4,4 +4,4 @@ alias sx="sudo xcode-select -s /Applications/Xcode.app"
 alias sxb="sudo xcode-select -s /Applications/Xcode-beta.app"
 alias ded="rm -rf ~/Library/Developer/Xcode/DerivedData/*"
 alias simreset="killall Xcode && sudo killall -9 com.apple.CoreSimulator.CoreSimulatorService && rm -rf ~/Library/Developer/CoreSimulator/Devices"
-
+alias play="open -a 'Xcode' ~/Development/Playground.playground"
